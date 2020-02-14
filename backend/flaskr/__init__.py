144c97b49aa3ad5bc75b613e1a9c6142afe1d4e6
DinaTaklit <ez_taklit@esi.dz>
2020-02-13 21:47:22 -0800

    feat: Implement error handler decorators for status codes 400, 404, and 422.

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -149,6 +149,47 @@ def create_book():
         print("\nerror => {}\n".format(error)) 
         abort(422)
 
+# @TODO: Review the above code for route handlers. 
+#        Pay special attention to the status codes used in the aborts since those are relevant for this task! 
+
+# @TODO: Write error handler decorators to handle AT LEAST status codes 400, 404, and 422. 
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success":False,
+        "error":404,
+        "message":"resource not found"
+    }),404
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+    }),422
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success":False,
+        "error":400,
+        "message": "bad request"
+    }),400
+    
+@app.errorhandler(405)
+def not_allowed(error):
+    return jsonify({
+        "success": False,
+        "error":405,
+        "message": "method not allowed"
+    }),405   
+# TEST: Practice writing curl requests. Write some requests that you know will error in expected ways.
+#       Make sure they are returning as expected. Do the same for other misformatted requests or requests missing data.
+#       If you find any error responses returning as HTML, write new error handlers for them. 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
