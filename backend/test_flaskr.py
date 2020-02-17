3f08e7d356621b9480f5a50839878a91b585a795
DinaTaklit <ez_taklit@esi.dz>
2020-02-17 08:16:08 -0800

    feat: Test: add setup method
        - add main function to the file
        - Correct the mistake of puting unittest.main() in the main of __init__ file instead of test_flaskr one.

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -12,8 +12,24 @@ class BookTestCase(unittest.TestCase):
     #Define and implement the setUp function. It will be executed before each test and is where you should initialize the app and test client, as well as any other context your tests will need. The Flask library provides a test client for the application, accessed as shown below.
     def setUp(self):
         """Define test variables and initialize app."""
-        self.client = app.test_client
-        pass
+        self.app = create_app() 
+        self.client = self.app.test_client
+        self.database_name = "bookself_test"
+        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+        
+        self.new_book = {
+            'title': 'Anansi Boys',
+            'author': 'Neil Gaiman',
+            'rating': 5
+        }
+        #binds the app to the current context 
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            # create all tables
+            self.db.create_all() 
+            
     #Define the tearDown method, which is implemented after each test. It will run as long as setUp executes successfully, regardless of test success.
     def tearDown(self):
         """ Executed after each test """
@@ -26,3 +42,13 @@ class BookTestCase(unittest.TestCase):
         """Test ____________ """
         res = self.client().get('/')
         self.assertEqual(res.status_code, 200)
+    
+    # @TODO: Write at least two tests for each endpoint - one each for success and error behavior.
+    #        You can feel free to write additional tests for nuanced functionality,
+    #        Such as adding a book without a rating, etc. 
+    #        Since there are four routes currently, you should have at least eight tests. 
+    # Optional: Update the book information in setUp to make the test database your own! 
+        
+#Run the test suite, by running python test_file_name.py from the command line.
+if __name__ == "__main__":
+    unittest.main()
