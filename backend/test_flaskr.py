db42362016ce0f602c10d4ad59b855ae042e0710
DinaTaklit <ez_taklit@esi.dz>
2020-02-17 10:52:34 -0800

    feat: Test: Implement search test using TDD logic
        - test search with results
        - test search without results

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -131,6 +131,30 @@ class BookTestCase(unittest.TestCase):
         self.assertEqual(data['success'], False)
         self.assertEqual(data['message'], 'method not allowed')
     
+    #Test-Driven Development (or TDD) is a software development paradigm used very commonly in production. It is based on a short, rapid development cycle in which tests are written before the executable code and constantly iterated on. 
+    # This part use this logic 
+    
+    # Test get books search with result
+    def test_get_book_search_with_results(self):
+        res = self.client().post('/books', json={'search':'Novel'})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(data['total_books'])
+        self.assertEqual(len(data['books']), 4)
+        
+    # Test get books search without result 
+    def test_get_book_search_without_results(self):
+        res = self.client().post('/books', json={'search':'applejacks'})
+        data = json.loads(res.data)
+        
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['total_books'], 0)
+        self.assertEqual(len(data['books'], 0))
+    
+    
 #Run the test suite, by running python test_file_name.py from the command line.
 if __name__ == "__main__":
     unittest.main()
